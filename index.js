1ee42a2c573a21205c5cadb2d2cf8646630190d8
devagya <rattandevagya@gmail.com>
2023-11-18 16:01:26 +0530

    all files added

---
+++ index.js
@@ -0,0 +1,51 @@
+const sliderImgs = [
+  "pexels-leah-kelley-3935702.jpg",
+  "pexels-leah-kelley-3935702.jpg",
+  "pexels-leah-kelley-3935702.jpg",
+  "pexels-leah-kelley-3935702.jpg",
+  "pexels-leah-kelley-3935702.jpg",
+  "pexels-leah-kelley-3935702.jpg",
+];
+let sliderImage = document.querySelector(".background-image");
+let sliderGrids = [...document.querySelectorAll(".grid-item")];
+
+let currentImage = 0;
+
+setInterval(() => {
+  changeSliderImage();
+}, 5000);
+
+const changeSliderImage = () => {
+  sliderGrids.map((gridItem, index) => {
+    setTimeout(() => {
+      gridItem.classList.remove("hide");
+
+      setTimeout(() => {
+        if (index == sliderGrids.length - 1) {
+          if (currentImage >= sliderImgs.length - 1) {
+            currentImage = 0;
+          } else {
+            currentImage++;
+          }
+
+          sliderImage.src = `img/${sliderImgs[currentImage]}`;
+
+          sliderGrids.map((item, i) => {
+            setTimeout(() => {
+              item.classList.add("hide");
+            }, i * 100);
+          });
+        }
+      }, 100);
+    }, index * 100);
+  });
+};
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  if (scrollY >= 188) {
+    navbar.classList.add("bg");
+  } else {
+    navbar.classList.remove("bg");
+  }
+});
